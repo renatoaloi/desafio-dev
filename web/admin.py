0fca8233b24113a1ab6da7c92cd648207f5dd459
Renato Aloi <renato.aloi@gmail.com>
2021-09-23 21:30:10 -0300

    fix: huge refactor for web backend admin

---
+++ web/admin.py
@@ -0,0 +1,42 @@
+"""Admin MVC"""
+from django.contrib import admin
+from django.utils.html import format_html_join
+from web.models import CnabTemplate, ImportTemplate, \
+    ImportCnabTemplate, TransactionTypeTemplate
+
+
+class ImportCnabTemplateInline(admin.TabularInline):
+    """ImportCnabTemplate Inline"""
+    model = ImportCnabTemplate
+    extra = 1
+
+
+class ImportTemplateAdmin(admin.ModelAdmin):
+    """ImportCnabTemplate Admin"""
+    inlines = (ImportCnabTemplateInline,)
+    list_display = ['description', 'get_templates']
+
+    def get_templates(self, obj):
+        """Get templates"""
+        return format_html_join(
+            '\n', "<li>{} - {}</li>",
+            ((o.description,o.commentary) for o in obj.template.all())
+        )
+
+    get_templates.short_description = "Campos CNAB"
+
+
+class CnabTemplateAdmin(admin.ModelAdmin):
+    """CnabTemplate Admin"""
+    list_display = ['description', 'start', 'end', 'size', 'commentary']
+
+
+class TransactionTypeTemplateAdmin(admin.ModelAdmin):
+    """TransactionTypeTemplate Admin"""
+    list_display = ['description', 'operation', 'signal']
+
+
+admin.site.register(TransactionTypeTemplate, TransactionTypeTemplateAdmin)
+admin.site.register(CnabTemplate, CnabTemplateAdmin)
+admin.site.register(ImportTemplate, ImportTemplateAdmin)
+
